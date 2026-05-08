bb1d5c3a0b8a0956050d8fa7289e21e78a37985a
Nicole <nmulvey12@gmail.com>
2026-05-07 21:10:28 -0700

    Fix: include generated wasm header and use local instance

--- qemu-rv32-semihosting/tests/04_function_table/main.c
+++ qemu-rv32-semihosting/tests/04_function_table/main.c
@@ -1,63 +1,54 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Forward declare the instance type
-typedef struct w2c_ft w2c_ft;
-
-extern uint32_t w2c_ft_add(w2c_ft* instance, uint32_t a, uint32_t b);
-extern uint32_t w2c_ft_subtract(w2c_ft* instance, uint32_t a, uint32_t b);
-extern uint32_t w2c_ft_multiply(w2c_ft* instance, uint32_t a, uint32_t b);
-extern uint32_t w2c_ft_call_op(w2c_ft* instance, uint32_t op_index, uint32_t a, uint32_t b);
-extern void wasm2c_ft_instantiate(w2c_ft* instance);
-
-// This will be defined in the generated wasm2c code
-extern w2c_ft w2c_ft_instance;
+// Include the generated WASM header
+#include "function_table.wasm.h"
 
 int main(void) {
     printf("Testing WASM function tables\n");
     
-    w2c_ft* instance = &w2c_ft_instance;
-    wasm2c_ft_instantiate(instance);
+    w2c_ft instance = {};
+    wasm2c_ft_instantiate(&instance);
     
-    uint32_t val = w2c_ft_add(instance, 3, 4);
+    uint32_t val = w2c_ft_add(&instance, 3, 4);
     if (val != 7) {
         printf("FAIL: Direct add(3, 4) = %lu, expected 7\n", val);
         return 1;
     }
     
-    val = w2c_ft_subtract(instance, 10, 3);
+    val = w2c_ft_subtract(&instance, 10, 3);
     if (val != 7) {
         printf("FAIL: Direct subtract(10, 3) = %lu, expected 7\n", val);
         return 1;
     }
     
-    val = w2c_ft_multiply(instance, 3, 4);
+    val = w2c_ft_multiply(&instance, 3, 4);
     if (val != 12) {
         printf("FAIL: Direct multiply(3, 4) = %lu, expected 12\n", val);
         return 1;
     }
     
-    val = w2c_ft_call_op(instance, 0, 5, 6);
+    val = w2c_ft_call_op(&instance, 0, 5, 6);
     if (val != 11) {
         printf("FAIL: Indirect call_op(0, 5, 6) = %lu, expected 11\n", val);
         return 1;
     }
     
-    val = w2c_ft_call_op(instance, 1, 10, 4);
+    val = w2c_ft_call_op(&instance, 1, 10, 4);
     if (val != 6) {
         printf("FAIL: Indirect call_op(1, 10, 4) = %lu, expected 6\n", val);
         return 1;
     }
     
-    val = w2c_ft_call_op(instance, 2, 3, 5);
+    val = w2c_ft_call_op(&instance, 2, 3, 5);
     if (val != 15) {
         printf("FAIL: Indirect call_op(2, 3, 5) = %lu, expected 15\n", val);
         return 1;
     }
     
-    uint32_t result1 = w2c_ft_call_op(instance, 0, 100, 50);
-    uint32_t result2 = w2c_ft_call_op(instance, 1, 100, 50);
-    uint32_t result3 = w2c_ft_call_op(instance, 2, 100, 50);
+    uint32_t result1 = w2c_ft_call_op(&instance, 0, 100, 50);
+    uint32_t result2 = w2c_ft_call_op(&instance, 1, 100, 50);
+    uint32_t result3 = w2c_ft_call_op(&instance, 2, 100, 50);
     
     if (result1 != 150 || result2 != 50 || result3 != 5000) {
         printf("FAIL: Multiple ops test - got (%lu, %lu, %lu), expected (150, 50, 5000)\n",
