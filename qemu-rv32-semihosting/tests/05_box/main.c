0dd196e0d85938f06424aa7a678198a9e52d4a72
Nicole <nmulvey12@gmail.com>
2026-05-11 18:28:39 -0700

    Add 05_box test: TweetNaCl crypto_box on bare-metal rv32

---
+++ qemu-rv32-semihosting/tests/05_box/main.c
@@ -0,0 +1,69 @@
+#include "tweetnacl.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define MSG_LEN 64
+#define ZERO_PAD 32
+#define BOX_PAD 16
+
+static uint32_t prng_state = 12345;
+static uint32_t prng_next(void) {
+    prng_state = prng_state * 1103515245 + 12345;
+    return (prng_state / 65536) % 32768;
+}
+
+void randombytes(unsigned char *x, unsigned long long xlen) {
+    for (unsigned long long i = 0; i < xlen; i++) {
+        x[i] = prng_next() & 0xFF;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+
+    printf("Testing TweetNaCl crypto_box\n\n");
+
+    unsigned char pk[32], sk[32];
+    unsigned char pk2[32], sk2[32];
+
+    printf("Generating keypairs...\n");
+    crypto_box_keypair(pk, sk);
+    crypto_box_keypair(pk2, sk2);
+
+    /* NaCl requires 32-byte zero padding before plaintext */
+    unsigned char msg[ZERO_PAD + MSG_LEN];
+    unsigned char cipher[ZERO_PAD + MSG_LEN];
+    unsigned char decrypted[ZERO_PAD + MSG_LEN];
+    unsigned char nonce[24];
+
+    printf("Preparing message and nonce...\n");
+    memset(msg, 0, ZERO_PAD);
+    memset(msg + ZERO_PAD, 0x42, MSG_LEN);
+    memset(nonce, 0x00, 24);
+
+    printf("Encrypting...\n");
+    int ret = crypto_box(cipher, msg, ZERO_PAD + MSG_LEN, nonce, pk2, sk);
+    if (ret != 0) {
+        printf("FAIL: crypto_box encryption failed\n");
+        return 1;
+    }
+    printf("PASS: crypto_box encryption succeeded\n");
+
+    printf("Decrypting...\n");
+    ret = crypto_box_open(decrypted, cipher, ZERO_PAD + MSG_LEN, nonce, pk, sk2);
+    if (ret != 0) {
+        printf("FAIL: crypto_box decryption failed\n");
+        return 1;
+    }
+    printf("PASS: crypto_box decryption succeeded\n");
+
+    if (memcmp(decrypted + ZERO_PAD, msg + ZERO_PAD, MSG_LEN) != 0) {
+        printf("FAIL: Decrypted message doesn't match original\n");
+        return 1;
+    }
+    printf("PASS: Decrypted message matches original\n\n");
+    printf("All crypto_box tests passed!\n");
+    return 0;
+}
---
+++ qemu-rv32-semihosting/tests/05_box/tweetnacl_wrapper.c
@@ -0,0 +1,5 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunterminated-string-initialization"
+#include "tweetnacl.c"
+#pragma GCC diagnostic pop
